5305432078416d7ec312e2985b58ca7600c9b150
Me <charlie_haddock@hotmail.co.uk>
2018-04-01 19:27:34 +0100

    Made player use the effects manager more intelligently

--- player/player.py
+++ player/player.py
@@ -1,46 +1,21 @@
-# Class to track status effects on players
-# statuses are (in no particular order):
-#  Confusion/Maladroit
-#  Amnesia
-#  Paralysis
-#  Magic Mirror
-#  Blindness
-#  Invisibility
-#  Charmed
-#
+from player import EffectsManager as ef
+from Utils import SpellChain
 
-from enum import Flag, auto
-
-
-class Statuses(Flag):
-    CONFUSION = auto()
-    AMNESIA = auto()
-    PARALYSIS = auto()
-    MAGIC_MIRROR = auto()
-    BLINDNESS = auto()
-    INVISIBILITY = auto()
-    CHARMED = auto()
-    DISEASED = auto()
-    POISONED = auto()
-    NO_EFFECTS = auto()
-    ANTI_SPELL = auto()
-    SURRENDERED = auto()
 
 allowed_gestures = "DPSFWdpsfwc>-"
 
 
-class Player:
+class Player(SpellChain):
+
+    # abstract method from SpellChain
+    def trigger_spell(self, spell):
+        spell.effect(self)
 
     def __init__(self, name):
         self.health = 10
         self.name = name
         self.alive = True
-        self.shielded = False
-        self.status = Statuses(Statuses.NO_EFFECTS)
-        self.turns_diseased = 0
-        self.turns_poisoned = 0
-        self.turns_invisible = 0
-        self.turns_blind = 0
+        self.effects = ef.EffectsManager(self)
 
         self.left_hand = ""
         self.right_hand = ""
@@ -59,39 +34,11 @@ class Player:
             print("{} dies!".format(self.name))
             self.alive = False
 
-    def set_shield(self, shield_on):
-        self.shielded = shield_on
-
-    def heal(self, heal_sum):
-        self.health += heal_sum
-
-    def set_invisible(self, turns):
-        self.turns_invisible = turns
-
-    def cure_disease(self):
-        self.status -= Statuses.DISEASED
-
-    def surrender(self):
-        self.status += Statuses.SURRENDERED
-
-    def anti_spell(self):
-        self.status+=Statuses.ANTI_SPELL
-
     def turn(self):
-        # reset status effects
-        self.status = Statuses.NO_EFFECTS
-
-        # resolve ongoing status effects
-        self.turns_invisible-=1
-        self.turns_blind-=1
-        self.turns_poisoned+=1
-        self.turns_diseased+=1
-
-        if self.turns_invisible > 0:
-            self.status += Statuses.INVISIBILITY
-        if self.turns_blind > 0:
-            self.status += Statuses.BLINDNESS
-
+        # resolve status effects
+        self.effects.resolve_effects()
+        # process other spells
+        self.check_dead()
 
     def make_left_gesture(self, gesture):
         if allowed_gestures.find(gesture) != -1:
